c1a63d29b3d536318386f78432543b4f104c7d46
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-05-18 10:41:50 -0500

    some more good stuff done today

--- app/Http/Controllers/Assets.php
+++ app/Http/Controllers/Assets.php
@@ -15,45 +15,6 @@ class Assets extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    // private function writeStream($resultobject, $session) {
-    //
-    // }
-
-    // public function index($slug)
-    // {
-    //   $cluster   = Cassandra::cluster()
-    //              ->withContactPoints(env('CASSANDRA_HOST'))// connects to localhost by default
-    //              ->build();
-    //   $keyspace  = env('CASSANDRA_KEYSPACE');
-    //   $session   = $cluster->connect($keyspace);        // create session, optionally scoped to a keyspace
-    //   $objectstatement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
-    //       'SELECT object_id, chunk_count FROM image WHERE image_url=\''.(string)$slug.'\''
-    //   );
-    //   $futureobject    = $session->executeAsync($objectstatement);  // fully asynchronous and easy parallel execution
-    //   $resultobject    = $futureobject->get();
-    //
-    //   $response = new StreamedResponse();
-    //   $callback = function ($resultobject) use ($session){
-    //     // foreach (range(1, (int)$resultobject[0]['chunk_count']) as $number) {
-    //     //     return $number;
-    //     // }
-    //     $chunkstatement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
-    //         'SELECT data, chunk_size FROM blob_chunk WHERE object_id=\''.$resultobject[0]['object_id'].'\''
-    //     );
-    //     $out = fopen('php://output', 'w');
-    //     $resultchunk = $session->execute($chunkstatement);  // fully asynchronous and easy parallel execution
-    //     fwrite($out, $resultchunk[0]['data'], $resultchunk[0]['chunk_size']);
-    //     fclose($out);
-    //   }
-    //   $response->setCallback($callback($resultobject));
-    //   return $response;
-    //
-    //
-    //
-    //
-    //
-    // }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -73,10 +34,7 @@ class Assets extends Controller
       $resultobject    = $futureobject->get();
 
       $response = new StreamedResponse(function() use($slug, $session, $resultobject){
-          // Open output stream
           $handle = fopen('php://output', 'w');
-          //
-
 
           foreach (range(1, (int)$resultobject[0]['chunk_count']) as $number) {
             $chunkstatement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
